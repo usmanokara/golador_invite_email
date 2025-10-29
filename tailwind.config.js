1ee3c8a6b4f3c66ebe87491a785d69ffe66494f1
Munizasaeed <munizasaeed85@gmail.com>
2025-10-29 10:10:30 -0700

    Added new feature X

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",          // all HTML files in the root
+    "./public/*.html"    // or wherever your HTML is
+  ],
   theme: {
     extend: {},
   },
